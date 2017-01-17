c40f386a83875b2f9c256d1f66d74ed0c58ba134
Fahim Shariar Shoumik <fahim.shoumik@gmail.com>
2017-01-18 04:17:33 +0600

    cleanup

---
+++ app/src/main/java/com/jabaldonedo/androidwifiscanner/Constants.java
@@ -0,0 +1,7 @@
+package com.jabaldonedo.androidwifiscanner;
+
+public class Constants {
+	public static final String APP_NAME = "ANDROID_WIFI_SCANNER";
+	public static final String INTENT_FILTER = "ANDROID_WIFI_SCANNER";
+	public static final String WIFI_DATA = "WIFI_DATA";
+}
